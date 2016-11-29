5ca9a9cabac6dd6311c1cf540221059f83d72660
Dan Finelli <dan.finelli@hotmail.com>
2016-11-28 16:32:34 -0800

    done

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,4 +1,20 @@
 console.log('app.js loaded!');
 
 // initialize the application
-var app = angular.module("hangmanApp", []);
+var app = angular.module("hangmanApp", [])
+              .controller('HangmanCtrl',HangmanCtrl);
+
+
+function HangmanCtrl(){
+  var vm = this;
+  var word = 'elephant';
+  vm.hangman = new HangmanGame(word, word.length);
+  vm.guessLetter = function (taco){
+      taco = taco.toLowerCase();
+      vm.hangman.guess(taco);
+      var input = document.getElementById('guess');
+      input.value = '';
+      if(vm.hangman.triesRemaining === 0){
+      }
+  }
+}
--- starter-code/js/hangman.js
+++ starter-code/js/hangman.js
@@ -2,47 +2,47 @@ console.log('hangmanGame loaded');
 
 // HangmanGame
 // to use:
-// 1. var game = new HangmanGame('supersecretword');
-// 2. game.guess('t');
+//  var game = new HangmanGame('supersecretword');
+//game.guess('t');
 var HangmanGame = function(secretWord, tries) {
-  this.secretWord = secretWord;
-  this.input = "";
-  this.guesses = [];
-  this.triesRemaining = tries || 7;
-  this.displayWord = this.filteredWord();
-  this.gameOver = false;
-  this.gameWon = null;
+    this.secretWord = secretWord;
+    this.input = "";
+    this.guesses = [];
+    this.triesRemaining = tries || 7;
+    this.displayWord = this.filteredWord();
+    this.gameOver = false;
+    this.gameWon = null;
 };
 
 // user can guess a letter
 HangmanGame.prototype.guess = function(guess) {
-  // don't continue if the game is over
-  if (this.gameOver) {
-    console.log("the game is over");
-    return false;
-  }
-  // check if the letter has already been guessed
-  var alreadyGuessed = this.guesses.indexOf(guess) !== -1;
-  if (alreadyGuessed) {
-    console.log("this letter has already been guessed");
+    // don't continue if the game is over
+    if (this.gameOver) {
+        console.log("the game is over");
+        return false;
+    }
+    // check if the letter has already been guessed
+    var alreadyGuessed = this.guesses.indexOf(guess) !== -1;
+    if (alreadyGuessed) {
+        console.log("this letter has already been guessed");
+        return;
+    }
+
+    // add the guess letter to the list of guesses
+    this.guesses.push(guess);
+
+    if (this.isLetterInWord(guess, this.secretWord)) {
+        // good guess
+        console.log('found ' + guess + ' in the word: ', this.secretWord);
+        // update display word
+        this.displayWord = this.filteredWord();
+    } else {
+        // bad guess
+        this.triesRemaining--;
+    }
+
+    this.checkForWinner();
     return;
-  }
-
-  // add the guess letter to the list of guesses
-  this.guesses.push(guess);
-
-  if (this.isLetterInWord(guess, this.secretWord)) {
-    // good guess
-    console.log('found ' + guess + ' in the word: ', this.secretWord);
-    // update display word
-    this.displayWord = this.filteredWord();
-  } else {
-    // bad guess
-    this.triesRemaining--;
-  }
-
-  this.checkForWinner();
-  return;
 };
 
 //////////////////////
@@ -51,35 +51,38 @@ HangmanGame.prototype.guess = function(guess) {
 
 // filteredWord returns the word completed up till now
 HangmanGame.prototype.filteredWord = function() {
-  var displayWord = '';
-  for (var index in this.secretWord) {
-    var currentLetter = this.secretWord[index];
-    if(this.guesses.indexOf(currentLetter) > -1) {
-      displayWord += currentLetter;
-    } else {
-      displayWord += '_';
+    var displayWord = '';
+    for (var index in this.secretWord) {
+        var currentLetter = this.secretWord[index];
+        if (this.guesses.indexOf(currentLetter) > -1) {
+            displayWord += currentLetter;
+        } else {
+            displayWord += '_';
+        }
     }
-  }
 
-  return displayWord;
+    return displayWord;
 };
 
 // determines win/lose status
 HangmanGame.prototype.checkForWinner = function() {
-  if(this.triesRemaining === 0) {
-    console.log("Sorry, you loose.")
-    this.gameOver = true;
-    this.gameWon = false;
-  // user wins if there are no more underscores in word
-  } else if( !this.isLetterInWord("_", this.displayWord) ) {
-    console.log("Yay, you win!")
-    this.gameOver = true;
-    this.gameWon = true;
-  }
+  var input = document.getElementsByClassName('glyphicon')[0];
+    if (this.triesRemaining === 0) {
+        console.log("Sorry, you loose.")
+        this.gameOver = true;
+        this.gameWon = false;
+        input.className += " glyphicon-remove";
+        // user wins if there are no more underscores in word
+    } else if (!this.isLetterInWord("_", this.displayWord)) {
+        input.className += " glyphicon-ok";
+        console.log("Yay, you win!")
+        this.gameOver = true;
+        this.gameWon = true;
+    }
 };
 
 // returns true if the letter is in the word, false if not
 HangmanGame.prototype.isLetterInWord = function(guess, word) {
-  // determine if the letter is in the word
-  return word.split('').indexOf(guess) > -1;
+    // determine if the letter is in the word
+    return word.split('').indexOf(guess) > -1;
 };
